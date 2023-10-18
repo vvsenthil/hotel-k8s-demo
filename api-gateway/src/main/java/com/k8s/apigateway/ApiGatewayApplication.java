de5a7577426791b9e076d649d7aef5142312afd4
vvsenthil <vvsenthil@gmail.com>
2023-10-18 10:56:46 +0100

    first commit

--- api-gateway/src/main/java/com/k8s/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/k8s/apigateway/ApiGatewayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		System.out.println("****************************");
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
