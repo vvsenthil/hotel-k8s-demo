72d3f19b238f55f4522196704c21a2cf93b737ca
vvsenthil <vvsenthil@gmail.com>
2023-10-18 10:29:45 +0100

    first commit

--- api-gateway/src/main/java/com/k8s/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/k8s/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.k8s.hotelk8sdemo;
+package com.k8s.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class HotelK8sDemoApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelK8sDemoApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
--- hotel-k8s-demo/src/main/java/com/k8s/hotelk8sdemo/controller/HotelController.java
+++ hotel-k8s-demo/src/main/java/com/k8s/hotelk8sdemo/controller/HotelController.java
@@ -1,9 +1,11 @@
 package com.k8s.hotelk8sdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hotel")
 public class HotelController {
     @GetMapping("/hello")
     public String sayHello(){
